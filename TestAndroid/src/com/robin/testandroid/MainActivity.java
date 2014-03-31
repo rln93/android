9dff2d328982f203d81371bf2e18b5db772e9dee
Robin Larsson <laxen93a@gmail.com>
2014-03-31 14:24:49 +0200

    Knapp med toast

--- TestAndroid/src/com/robin/testandroid/MainActivity.java
+++ TestAndroid/src/com/robin/testandroid/MainActivity.java
@@ -1,16 +1,31 @@
 package com.robin.testandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 // Tog bort alla tidigare kommentarerna
 public class MainActivity extends Activity {
 
+	private Button hejBtn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		hejBtn = (Button)findViewById(R.id.hej_button);
+		hejBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, R.string.hej_knapp, Toast.LENGTH_SHORT).show();
+				
+			}
+		});
 	}
 
 	@Override
